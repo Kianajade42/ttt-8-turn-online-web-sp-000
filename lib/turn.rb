14233d5426c236c26e4b2ee4e8ace2b80696cfc5
Kiana Long <kianajade42.kl@gmail.com>
2021-01-13 21:09:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,13 +28,3 @@ def move(board, index, character="X")
   puts board[index]=character
   end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    player_move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
